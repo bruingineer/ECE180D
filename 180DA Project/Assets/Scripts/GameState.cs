d48e75ff4ad85e3947cff557b43eb67ac18deba3
Nicolas Magana <nmagana2112@gmail.com>
2018-11-19 12:43:55 -0800

    Added changes to GameState

---
+++ 180DA Project/Assets/Scripts/GameState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+	public int numLanes;
+	public static int middle_lane;
+	
+	void Start () {
+		// change to make it dynamic
+		numLanes = 10;
+		middle_lane = numLanes/2;
+	}
+	
+	// // Update is called once per frame
+	// void Update () {
+		
+	// }
+}
