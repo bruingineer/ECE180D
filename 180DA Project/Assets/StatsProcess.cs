095bb4c102b4bf8918743786bb42d77118f8c41e
brayan2800 <brayan2800@gmail.com>
2018-12-04 23:26:17 -0800

    End Scene Updates

--- 180DA Project/Assets/StatsProcess.cs
+++ 180DA Project/Assets/StatsProcess.cs
@@ -12,14 +12,40 @@ using System.Text;
 
 public class StatsProcess : MonoBehaviour {
 
+    private MqttClient client;
+    private const string str_IP = "127.0.0.1";
+    private const int int_Port = 1883;
+    private const string topic = "database";
+
     public Text gesture_acc;
     public Text speech_acc;
     public Text n_hits;
+    float g;
+    float s;
+    float n;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //g = SelectedPlayer.current_gesture_pass / (SelectedPlayer.current_gesture_fail + SelectedPlayer.current_gesture_pass);
+        //s = SelectedPlayer.current_speech_pass / (SelectedPlayer.current_speech_fail + SelectedPlayer.current_speech_pass);
+        //n = SelectedPlayer.current_hits; 
+        g = 0.5f;
+        s = 0.88f;
+        n = 10f;
+
+        gesture_acc.text += ("  " + g);
+        speech_acc.text += ("  " + s);
+        n_hits.text += ("  " + n);
+
+        int game_idx = SelectedPlayer.games_played + 1;
+        SelectedPlayer.games_played++;
+
+        string values = String.Format("(\'{0}\', \'{0}\', \'{0}\', \'{0}\', \'{0}\' )",
+                                    SelectedPlayer.id, game_idx, g, s, n);
+        //string str_command = "INSERT INTO games (player, player_game_idx, gestures_acc, speech_acc, n_hits)
+        //                                  VALUES  ";
+        //byte[] command = Encoding.ASCII.GetBytes(str_command);
+    }
 	
 	// Update is called once per frame
 	void Update () {
