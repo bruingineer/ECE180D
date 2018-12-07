535859a129f362b829de49b9948f6bd3eb3919c8
brayan2800 <brayan2800@gmail.com>
2018-12-07 10:56:27 -0800

    Removing unnecessary get/set functions

--- 180DA Project/Assets/Scripts/SelectedPlayer.cs
+++ 180DA Project/Assets/Scripts/SelectedPlayer.cs
@@ -2,49 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public static class SelectedPlayer{
     
     //player info
     public static string name;
     public static int id, games_played;
 
-    public static string Name
-    {
-        get
-        {
-            return name;
-        }
-        set
-        {
-            name = value;
-        }
-    }
+    //public static string Name
+    //{
+    //    get
+    //    {
+    //        return name;
+    //    }
+    //    set
+    //    {
+    //        name = value;
+    //    }
+    //}
 
-    public static int Id
-    {
-        get
-        {
-            return id;
-        }
-        set
-        {
-            id = value;
-        }
-    }
+    //public static int Id
+    //{
+    //    get
+    //    {
+    //        return id;
+    //    }
+    //    set
+    //    {
+    //        id = value;
+    //    }
+    //}
 
-    public static int Games_played
-    {
-        get
-        {
-            return games_played;
-        }
-        set
-        {
-            games_played = value;
-        }
-    }
+    //public static int Games_played
+    //{
+    //    get
+    //    {
+    //        return games_played;
+    //    }
+    //    set
+    //    {
+    //        games_played = value;
+    //    }
+    //}
 
     //current game info
     public static int current_hits = 0;
@@ -62,5 +60,5 @@ public static class SelectedPlayer{
         current_speech_pass = 0;
         current_speech_fail = 0;
         bigLaserctr = 0;
-}
+    }
 }
