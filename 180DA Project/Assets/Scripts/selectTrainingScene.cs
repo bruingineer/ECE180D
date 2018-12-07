09976f313aa27e96ac72d99f4215dc60675c6fb1
brayan2800 <brayan2800@gmail.com>
2018-12-07 09:35:13 -0800

    Implemented Training Button

---
+++ 180DA Project/Assets/Scripts/selectTrainingScene.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class selectTrainingScene : MonoBehaviour {
+
+    public Text training_suggestion;
+    public Button trainingButton;
+
+    // Use this for initialization
+    void Start () {
+        trainingButton = GetComponent<Button>();
+        trainingButton.onClick.AddListener(ChooseScene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ChooseScene()
+    {
+        string txt = training_suggestion.text;
+        if ( txt != null)
+        {
+            if (txt.Contains("gesture"))
+            {
+                Debug.Log("Changing to gesture training");
+                SceneManager.LoadScene("gestureTraining");
+            }
+
+            else if (txt.Contains("speech"))
+            {
+                Debug.Log("Changing to speech training");
+                SceneManager.LoadScene("speechTraining");
+            }
+
+            else if (txt.Contains("dodging"))
+            {
+                Debug.Log("Changing to laser training");
+                SceneManager.LoadScene("laserTraining");
+            }
+        }
+    }
+}
