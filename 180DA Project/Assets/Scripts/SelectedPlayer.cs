2c9d6c1338dbe819e1526d38e80d1ef8bfa7357b
brayan2800 <brayan2800@gmail.com>
2018-12-07 17:50:07 -0800

    Revert "Revert "Merged updated""
    This reverts commit 1f1ef73c4c6255abfb69abb335cc8dad7a85f3ec.

--- 180DA Project/Assets/Scripts/SelectedPlayer.cs
+++ 180DA Project/Assets/Scripts/SelectedPlayer.cs
@@ -2,52 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public static class SelectedPlayer{
     
+    //player info
     public static string name;
     public static int id, games_played;
 
-    public static string Name
-    {
-        get
-        {
-            return name;
-        }
-        set
-        {
-            name = value;
-        }
-    }
+    //public static string Name
+    //{
+    //    get
+    //    {
+    //        return name;
+    //    }
+    //    set
+    //    {
+    //        name = value;
+    //    }
+    //}
 
-    public static int Id
-    {
-        get
-        {
-            return id;
-        }
-        set
-        {
-            id = value;
-        }
-    }
+    //public static int Id
+    //{
+    //    get
+    //    {
+    //        return id;
+    //    }
+    //    set
+    //    {
+    //        id = value;
+    //    }
+    //}
 
-    public static int Games_played
-    {
-        get
-        {
-            return games_played;
-        }
-        set
-        {
-            games_played = value;
-        }
-    }
+    //public static int Games_played
+    //{
+    //    get
+    //    {
+    //        return games_played;
+    //    }
+    //    set
+    //    {
+    //        games_played = value;
+    //    }
+    //}
 
+    //current game info
     public static int current_hits = 0;
     public static int current_gesture_pass = 0;
     public static int current_gesture_fail = 0;
     public static int current_speech_pass = 0;
     public static int current_speech_fail = 0;
+    public static int bigLaserctr = 0;
+
+    public static void resetGameStats()
+    {
+        current_hits = 0;
+        current_gesture_pass = 0;
+        current_gesture_fail = 0;
+        current_speech_pass = 0;
+        current_speech_fail = 0;
+        bigLaserctr = 0;
+    }
 }
--- 180DA Project/Assets/Scripts/SpeechGame/HandleWordDisplay.cs
+++ 180DA Project/Assets/Scripts/SpeechGame/HandleWordDisplay.cs
@@ -1,36 +1,108 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-
-//This is wrapper class to the DisplayWord class
-//to hide the details and make it easier to use
 public class HandleWordDisplay : MonoBehaviour {
 
-
-    public DisplayWord DisplayWord;
-    public static int x;
+    public Vector3 InitPosition;
+    public static int Offset = 25;
 
     public void WhichWord(string w)
     {
-        //print(w);
-        DisplayWord.SetWord(w);
+        SetWord(w);
     }
 
     // Use this for initialization
     public void Display()
     {
-        DisplayWord.LoadWord();
+        LoadWord();
     }
         
     public void ResetDisplay()
     {
-        DisplayWord.ResetWord();
+        ResetWord();
     }
 
     public void RemoveDisplay()
     {
-        DisplayWord.DeleteWord();
+        DeleteWord();
     }
 
+    //Moved from DisplayWord.cs in order to reduce the number of Gameobjects needed
+    private string word;
+    private List<GameObjectStruct> wordList = new List<GameObjectStruct>();
+    private List<Vector3> initPositions = new List<Vector3>();
+    
+
+    public void SetWord(string w)
+    {
+        word = w;
+    }
+
+    public void LoadWord()
+    {
+        word = word.ToUpper();
+        Set_wordDict();
+    }
+
+    private void Set_wordDict()
+    {
+        print(word);
+        print(Offset);
+        int letter = 0;
+        foreach (char c in this.word)
+        {
+            GameObjectStruct myGameObjectStruct = new GameObjectStruct { myGameObject = LoadLetter.GetLetter(c) };
+            myGameObjectStruct.myGameObject.transform.localScale -= new Vector3(0.75f, 0.75f, 0);
+            myGameObjectStruct.myGameObject.transform.SetParent(this.transform);
+            Vector3 pos = InitPosition;
+            pos[0] = pos[0] + Offset * letter;
+            letter++;
+            myGameObjectStruct.myGameObject.transform.position = pos;
+            myGameObjectStruct.initPos = pos;
+            initPositions.Add(pos);
+            wordList.Add(myGameObjectStruct);
+        }
+
+        //Now Randomize initPostions
+        for (int i = 0; i < initPositions.Count; i++)
+        {
+            Vector3 temp = initPositions[i];
+            int randomIndex = Random.Range(i, initPositions.Count);
+            initPositions[i] = initPositions[randomIndex];
+            initPositions[randomIndex] = temp;
+        }
+
+        for (int i = 0; i < wordList.Count; i++)
+        {
+            wordList[i].myGameObject.transform.position = initPositions[i];
+        }
+    }
+
+    //Reset the original position of the word
+    public void ResetWord()
+    {
+        for (int i = 0; i < initPositions.Count; i++)
+        {
+            wordList[i].myGameObject.transform.position = wordList[i].initPos;
+        }
+    }
+
+    public void DeleteWord()
+    {
+        for (int i = 0; i < wordList.Count; i++)
+        {
+            Destroy(wordList[i].myGameObject);
+        }
+        wordList.Clear();
+    }
+
+
+}
+
+public struct GameObjectStruct
+{
+    public GameObject myGameObject;
+    public Vector3 initPos;
 }
