7273b722b65152c7f72452635a318bb36ddd7617
Jose <joseflores2395@gmail.com>
2018-12-09 15:28:24 -0800

    Added time functionality to GestureGame
    added Timer to Canvas (need to Revert in Unity)
    added tag to Timer

--- 180DA Project/Assets/Scripts/GestureGame.cs
+++ 180DA Project/Assets/Scripts/GestureGame.cs
@@ -7,22 +7,57 @@ using uPLibrary.Networking.M2Mqtt.Utility;
 using uPLibrary.Networking.M2Mqtt.Exceptions;
 using System.Net;
 
+using UnityEngine.UI;
+
 public class GestureGame : MonoBehaviour {
 	private List<string> gestures;
 	public static bool correctGestureReceived;
+
+	public static int numSucess;
+	public static int numFails;
+
+	private Text timeLeft;
+
 	// Use this for initialization
 	void Awake () {
 		correctGestureReceived = false;
 		gestures = new List<string>(){"tpose"};
 		GestureClient.gestureClient.Publish(GestureClient.topicGestureSent, System.Text.Encoding.UTF8.GetBytes(gestures[Random.Range(0, gestures.Count)]), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+		StartCoroutine("Timer");
 	}
 
 	void Update()
 	{
 		if (correctGestureReceived)
 		{
+			numSucess++;
+			StopCoroutine("Timer");
 			StartCoroutine(HandleCorrectGesture());
 		}
+	}
+
+	public IEnumerator Timer() 
+	{	
+			timeLeft = GameObject.FindWithTag("timer").GetComponent<Text>();
+            float duration = 8f;
+            while(duration >= 0)
+            {   
+                duration -= Time.deltaTime;
+                int integer = (int)duration;
+                if (integer >= 1)
+                    timeLeft.text = integer.ToString();
+                else
+                {
+                    timeLeft.text = "Time's Up";
+                }
+                yield return null;
+            }
+            
+            yield return new WaitForSeconds(1);
+			PlayerEvents.eventOn = false;
+
+			timeLeft.text = "";
+            Destroy(gameObject);
 	}
 
 	private IEnumerator HandleCorrectGesture()
