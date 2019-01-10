563717cc86d11afc37227b140d7388735dbfbff9
brayan2800 <brayan2800@gmail.com>
2019-01-10 14:51:08 -0800

    Removing unnecessary getters/setters

--- 180DA Project/Assets/Scripts/SelectedPlayer.cs
+++ 180DA Project/Assets/Scripts/SelectedPlayer.cs
@@ -8,42 +8,6 @@ public static class SelectedPlayer{
     public static string name;
     public static int id, games_played;
 
-    //public static string Name
-    //{
-    //    get
-    //    {
-    //        return name;
-    //    }
-    //    set
-    //    {
-    //        name = value;
-    //    }
-    //}
-
-    //public static int Id
-    //{
-    //    get
-    //    {
-    //        return id;
-    //    }
-    //    set
-    //    {
-    //        id = value;
-    //    }
-    //}
-
-    //public static int Games_played
-    //{
-    //    get
-    //    {
-    //        return games_played;
-    //    }
-    //    set
-    //    {
-    //        games_played = value;
-    //    }
-    //}
-
     //current game info
     public static int current_hits = 0;
     public static int current_gesture_pass = 0;
