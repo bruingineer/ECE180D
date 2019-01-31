aca7a6bd1feb14c1fe44315723475fdf079307f8
floresjd <28540828+floresjd@users.noreply.github.com>
2019-01-31 03:48:02 -0800

    trivia game

---
+++ 180DA Project/Assets/Scripts/SpeechGame/Trivia.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class Trivia : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_Hypotheses;
+
+    [SerializeField]
+    private Text m_Recognitions;
+
+    private DictationRecognizer m_DictationRecognizer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            m_Recognitions.text += text + "\n";
+        };
+
+        m_DictationRecognizer.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+            m_Hypotheses.text += text;
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+
+        m_DictationRecognizer.Start();
+
+        // string ques = "1";
+        // string ans = "1";
+        // TriviaList.getQuestion(ref ques, ref ans);
+        // Debug.Log(ques + ans);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // string ques = "1";
+        // string ans = "1";
+        // TriviaList.getQuestion(ref ques, ref ans);
+        // Debug.Log(ques + ans);
+    }
+}
---
+++ 180DA Project/Assets/Scripts/SpeechGame/TriviaList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class TriviaList : MonoBehaviour
+{
+    public static int size = 2;
+    public static IDictionary <string, string> triviaDictionay = new Dictionary<string, string>()
+    {
+        {"George Washingtion", "Who was the first US President?"},
+        {"Court of Sciences", "Patio in front of Boelter Hall is called?"}
+    };
+
+    public static void getQuestion(ref string question, ref string answer){
+        int r = Random.Range(0, size);
+        question = triviaDictionay.Keys.ElementAt(r);
+        answer = triviaDictionay[question];
+    }
+}
