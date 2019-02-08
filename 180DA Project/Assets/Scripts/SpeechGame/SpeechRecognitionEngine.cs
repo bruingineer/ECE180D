0f52b1fd51728437bb9a89824b5bbf070ec9244d
floresjd <28540828+floresjd@users.noreply.github.com>
2019-02-07 16:30:03 -0800

    commeted out SR

--- 180DA Project/Assets/Scripts/SpeechGame/SpeechRecognitionEngine.cs
+++ 180DA Project/Assets/Scripts/SpeechGame/SpeechRecognitionEngine.cs
@@ -1,123 +1,123 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Windows.Speech;
-using UnityEngine.UI;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.Windows.Speech;
+// using UnityEngine.UI;
 
-public class SpeechRecognitionEngine : Event
-{
-    // Objects
-    public HandleWordDisplay HandleWordDisplay;
-    public ConfidenceLevel confidence = ConfidenceLevel.Medium;
-    protected static PhraseRecognizer recognizer;
-    public Text scramble;
-    public string[] keywords = { "" };
-    private float endDisplayTime = 1.1f;
+// public class SpeechRecognitionEngine : Event
+// {
+//     // Objects
+//     public HandleWordDisplay HandleWordDisplay;
+//     public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+//     protected static PhraseRecognizer recognizer;
+//     public Text scramble;
+//     public string[] keywords = { "" };
+//     private float endDisplayTime = 1.1f;
     
 
-    //This will update the keywords that the speech recognizer will recognize
-    //For the list of avaiable words see WordList.cs
+//     //This will update the keywords that the speech recognizer will recognize
+//     //For the list of avaiable words see WordList.cs
 
-    //To select which word
-    public void WhichWord(int i)
-    {
-        keywords[0] = WordList.WordList_[i];
-        HandleWordDisplay.WhichWord(keywords[0]);
-    }
+//     //To select which word
+//     public void WhichWord(int i)
+//     {
+//         keywords[0] = WordList.WordList_[i];
+//         HandleWordDisplay.WhichWord(keywords[0]);
+//     }
 
-    //To randomize which word
-    public void ChooseRandWord()
-    {
-        System.Random rnd = new System.Random();
-        keywords[0] = WordList.WordList_[rnd.Next(WordList.WordList_.Length)];
-        //print(keywords[0]);
-        HandleWordDisplay.WhichWord(keywords[0]);
-    }
+//     //To randomize which word
+//     public void ChooseRandWord()
+//     {
+//         System.Random rnd = new System.Random();
+//         keywords[0] = WordList.WordList_[rnd.Next(WordList.WordList_.Length)];
+//         //print(keywords[0]);
+//         HandleWordDisplay.WhichWord(keywords[0]);
+//     }
 
-    //Can maybe use enable/disable
+//     //Can maybe use enable/disable
     
 
-    protected override void Initialize()
-    {
-        scramble.transform.position = timeLeft.transform.position + new Vector3(-450, 0, 0);
-        HandleWordDisplay.InitPosition = scramble.transform.position - new Vector3(80, 35, 0);
-    }
+//     protected override void Initialize()
+//     {
+//         scramble.transform.position = timeLeft.transform.position + new Vector3(-450, 0, 0);
+//         HandleWordDisplay.InitPosition = scramble.transform.position - new Vector3(80, 35, 0);
+//     }
 
-    protected override void SetUpEvent()
-	{
-		ChooseRandWord();
-        HandleWordDisplay.Display();
-        if (keywords != null)
-        {
-            recognizer = new KeywordRecognizer(keywords, confidence);
-            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
-            recognizer.Start();
-        }
-	}
+//     protected override void SetUpEvent()
+// 	{
+// 		ChooseRandWord();
+//         HandleWordDisplay.Display();
+//         if (keywords != null)
+//         {
+//             recognizer = new KeywordRecognizer(keywords, confidence);
+//             recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+//             recognizer.Start();
+//         }
+// 	}
 
-    protected override IEnumerator MakeTextBlink()
-    {
-        yield return HandleWordDisplay.MakeBlink(repeatRate);
-    }
+//     protected override IEnumerator MakeTextBlink()
+//     {
+//         yield return HandleWordDisplay.MakeBlink(repeatRate);
+//     }
 
-    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
-    {
-            timerPaused = true;
-            m_player.MovePlayer();
-            scramble.text = "Correct!";
-            StopRecognizer();
-            SelectedPlayer.current_speech_pass++;
-            HandleCorrectMiniGame();
-            StartCoroutine(Reset());
-    }
+//     private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+//     {
+//             timerPaused = true;
+//             m_player.MovePlayer();
+//             scramble.text = "Correct!";
+//             StopRecognizer();
+//             SelectedPlayer.current_speech_pass++;
+//             HandleCorrectMiniGame();
+//             StartCoroutine(Reset());
+//     }
 
-    IEnumerator Reset()
-    {
-        HandleWordDisplay.ResetDisplay();
-        //Think we can display the word for a few second then remove it from the display
-        yield return new WaitForSeconds(endDisplayTime);
-        //To remove the word from the display
-        HandleWordDisplay.RemoveDisplay();
-        //to stop the PhraseRecognizer
-        yield return StartCoroutine(DelayAndEndTimer());
-    }
+//     IEnumerator Reset()
+//     {
+//         HandleWordDisplay.ResetDisplay();
+//         //Think we can display the word for a few second then remove it from the display
+//         yield return new WaitForSeconds(endDisplayTime);
+//         //To remove the word from the display
+//         HandleWordDisplay.RemoveDisplay();
+//         //to stop the PhraseRecognizer
+//         yield return StartCoroutine(DelayAndEndTimer());
+//     }
 
-    protected override IEnumerator HandleIncorrect()
-	{
-        StopRecognizer();
-        HandleIncorrectMiniGame();
-		SelectedPlayer.current_speech_fail++;
-        yield return StartCoroutine(DelayAndEndTimer());
-	}
+//     protected override IEnumerator HandleIncorrect()
+// 	{
+//         StopRecognizer();
+//         HandleIncorrectMiniGame();
+// 		SelectedPlayer.current_speech_fail++;
+//         yield return StartCoroutine(DelayAndEndTimer());
+// 	}
 
-    private void OnApplicationQuit()
-    {
-        StopRecognizer();
-    }
+//     private void OnApplicationQuit()
+//     {
+//         StopRecognizer();
+//     }
 
-    private void StopRecognizer()
-    {
-        if (recognizer != null && recognizer.IsRunning)
-        {   
-            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
-            recognizer.Stop();
-            recognizer.Dispose();
-        }
-    }
+//     private void StopRecognizer()
+//     {
+//         if (recognizer != null && recognizer.IsRunning)
+//         {   
+//             recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+//             recognizer.Stop();
+//             recognizer.Dispose();
+//         }
+//     }
 
-    protected virtual void HandleCorrectMiniGame() {}
-    protected virtual void HandleIncorrectMiniGame() {}
+//     protected virtual void HandleCorrectMiniGame() {}
+//     protected virtual void HandleIncorrectMiniGame() {}
     
-}
+// }
 
-public class SpeechRecognitionEngine_Minigame : SpeechRecognitionEngine {
-	protected override void HandleCorrectMiniGame()
-	{
-		GetComponent<PlayerEvents_Gesture_Minigame>().curCorrect++;
-	}
+// public class SpeechRecognitionEngine_Minigame : SpeechRecognitionEngine {
+// 	protected override void HandleCorrectMiniGame()
+// 	{
+// 		GetComponent<PlayerEvents_Gesture_Minigame>().curCorrect++;
+// 	}
 
-	protected override void HandleIncorrectMiniGame()
-	{
-		GetComponent<PlayerEvents_Gesture_Minigame>().curCorrect = 0;
-	}
-}
+// 	protected override void HandleIncorrectMiniGame()
+// 	{
+// 		GetComponent<PlayerEvents_Gesture_Minigame>().curCorrect = 0;
+// 	}
+// }
