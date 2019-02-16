e9b80c3edd57d33a98f9e3b1d1bd75b421d4def1
Nicolas Magana <nmagana2112@gmail.com>
2019-02-15 18:29:19 -0800

    player scripts

---
+++ 180DA Project/Assets/Scripts/Game/Player/PlayerMQTT_X.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+
+using System;
+
+public class PlayerMQTT_X : MQTT_Class {
+	public int cur_lane_num, numLanes;
+	public PlayerMQTT_X(string topic, int numL) : base(topic)
+	{
+		cur_lane_num = numL / 2;
+		numLanes = numL;
+	}
+	
+	protected override void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
+	{ 
+		int lane_num;
+		if (Int32.TryParse(System.Text.Encoding.UTF8.GetString(e.Message), out lane_num)) {
+			lane_num -= 1;
+			if (lane_num != cur_lane_num) {
+				cur_lane_num = lane_num;
+			}
+		} 
+	} 
+
+}
