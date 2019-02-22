ec78afb126781ba89e7d68b769b5c17cd2f90d6e
floresjd <28540828+floresjd@users.noreply.github.com>
2019-02-21 17:38:22 -0800

    Split trivia and word scramble with Speech as the base class

---
+++ 180DA Project/Assets/Scripts/Game/Events/SpeechGame/Trivia.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+using TMPro;
+public class Trivia : Speech {
+	
+	private TextMeshProUGUI triviaText;
+	private TextMeshProUGUI answer;
+	private Text Question;
+
+	private string ques = "";
+	private string ans = "";
+
+	protected override void Initialize(){
+        triviaText = GameObject.FindWithTag("trivia").GetComponent<TextMeshProUGUI>();
+        triviaText.text = "";
+        answer = GameObject.FindWithTag("answer").GetComponent<TextMeshProUGUI>();
+        answer.text = "";
+        SetUp();
+    }
+
+	protected override void handleSpeechTask(string text){
+		answer.text = "";
+		answer.text = text;
+		if (answer.text == ans){
+			timerPaused = true;
+			m_player.MovePlayer();
+			triviaText.text = "Correct!";
+			StopRecognizer();
+			SelectedPlayer.current_trivia_pass++; //need to change
+			StartCoroutine("Reset");
+		}
+	}
+
+	protected override void SetUpEvent(){
+		Debug.Log("Starting trivia");
+		SpeechList.getQuestion(ref ques, ref ans);
+		Debug.Log("Question: " + ques);
+		Debug.Log("Answer: " + ans);
+		triviaText.text = ques;
+		Debug.Log(triviaText.text);
+		StartRecognizer();
+		m_DictationRecognizer.Start();
+	}
+
+	protected override IEnumerator Reset(){
+	    yield return new WaitForSeconds(endDisplayTime);
+        triviaText.text = "";
+        answer.text = "";
+        yield return StartCoroutine("Delay");
+        eventCorrect = true;
+	}
+
+	protected override IEnumerator HandleIncorrect(){
+        timerPaused = true;
+        StopRecognizer();
+        SelectedPlayer.current_trivia_fail++;
+        answer.text = "";
+        triviaText.text = "";
+        yield return StartCoroutine("Delay");
+    }
+
+	protected override IEnumerator MakeTextBlink(){
+		return null;
+	}	
+}
---
+++ 180DA Project/Assets/Scripts/Game/Events/SpeechGame/WordScramble.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+using TMPro;
+
+public class WordScramble : Speech {
+
+	WordDisplay WDisplay;
+	private TextMeshProUGUI Answer;
+	
+	private static int failed = 0;
+
+	protected override void handleSpeechTask(string text){
+		Answer.text = "";
+		Answer.text = text;
+		Debug.Log(text);
+		if (Answer.text == WDisplay.word_str)
+		{
+			timerPaused = true;
+			m_player.MovePlayer();
+			WDisplay.WordText.text = "Correct!";
+			StopRecognizer();
+			SelectedPlayer.current_unscramble_pass++;
+			StartCoroutine("Reset");
+		}
+	}
+
+	protected override void Initialize(){
+        WDisplay = new WordDisplay();
+		WDisplay.WordText = GameObject.FindWithTag("trivia").GetComponent<TextMeshProUGUI>();
+        WDisplay.WordText.text = "";
+        Answer = GameObject.FindWithTag("answer").GetComponent<TextMeshProUGUI>();
+        Answer.text = "";
+        SetUp();
+    }
+
+	protected override void SetUpEvent(){       
+		Debug.Log("Starting Scramble");
+		string w = "";
+		w = SpeechList.getWord();
+		WDisplay.SetWordDisplay(w);
+		WDisplay.SetFirstLetterClue();
+		Debug.Log(w);
+		Debug.Log(WDisplay.WordText.text);
+		StartRecognizer();
+		m_DictationRecognizer.Start();
+    }
+
+	protected override IEnumerator Reset(){
+		yield return new WaitForSeconds(endDisplayTime);
+        WDisplay.WordText.text = "";
+        Answer.text = "";
+        yield return StartCoroutine("Delay");
+        eventCorrect = true;
+	}
+
+	protected override IEnumerator MakeTextBlink(){
+            while(!timerPaused){
+            //Debug.Log("making word blink");
+            WDisplay.MakeWordBlink();
+			yield return new WaitForSeconds(repeatRate/1.5f);
+        }
+    }
+
+	protected override IEnumerator HandleIncorrect(){
+        timerPaused = true;
+        StopRecognizer();
+        SelectedPlayer.current_unscramble_fail++;
+        WDisplay.WordText.text = "";
+        Answer.text = "";
+        yield return StartCoroutine("Delay");
+    }
+
+}
