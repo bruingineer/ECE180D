66d65470fa7d4a453678e70e00b8364ecf616669
bruingineer <joshcamarena@ucla.edu>
2019-03-07 16:24:27 -0800

    added keypoints module

--- gestures/multiplayer-openpose.py
+++ gestures/multiplayer-openpose.py
@@ -9,13 +9,14 @@ from time import time
 
 import numpy as np 
 import paho.mqtt.client as mqtt
+import keypointFrames
 
 # contants to enable debug options
 # TODO: add these to the arguments
 DEBUG_MQTT = False
 DEBUG_MAIN = False
 DEBUG_PROCESS_KEYPOINTS = False
-MQTT_ENABLE = True
+MQTT_ENABLE = False
 
 waiting_for_target = True
 playerheader = 'A' # or B
@@ -70,207 +71,6 @@ def connect_to_server(_ip, _port):
     client.connect(_ip, _port, 60)
     client.subscribe(target_topic, qos=0)
     return client
-
-# dictionary to convert body part names to the body_25 indexes
-body25 = {
-        "Nose":      0,
-        "Neck":      1,
-        "RShoulder": 2,
-        "RElbow":    3,
-        "RWrist":    4,
-        "LShoulder": 5,
-        "LElbow":    6,
-        "LWrist":    7,
-        "MidHip":    8,
-        "RHip":      9,
-        "RKnee":    10,
-        "RAnkle":   11,
-        "LHip":     12,
-        "LKnee":    13,
-        "LAnkle":   14,
-        "REye":     15,
-        "LEye":     16,
-        "REar":     17,
-        "LEar":     18,
-        "LBigToe":  19,
-        "LSmallToe":20,
-        "LHeel":    21,
-        "RBigToe":  22,
-        "RSmallToe":23,
-        "RHeel":    24,
-        "Background":25
-    }
-
-class keypointFrames:
-    """
-    reference for coordinates given in keypoints np array
-    (WIDTH,0)---------(0,0)
-          |             |
-          |             |
-    (WIDTH,HEIGHT)----(0,HEIGHT)
-
-    keypoints shape [people x parts x index] == (1L, 25L, 3L)
-    index[] = [x , y , confidence]
-    """
-
-    def __init__(self):
-        self.last_3_frames = []
-
-    def add(self, _keypoints, inputWIDTH, inputHEIGHT):
-        self.keypoints = np.array(_keypoints)
-        # print(self.keypoints)
-        # print('*********')
-        if len(self.last_3_frames) < 3:
-            self.last_3_frames.append((time(),self.keypoints))
-            #print(self.last_3_frames)
-        else:
-            self.last_3_frames.pop(0)
-            self.last_3_frames.append((time(),self.keypoints))
-
-        #np.array(keypoints)
-        # self.num_people = self.keypoints.shape[1]
-        self.WIDTH = inputWIDTH
-        self.HEIGHT = inputHEIGHT
-
-    def checkFor(self, _target_gesture):
-        #print ("CheckFor " + _target_gesture)
-        _target_gesture = _target_gesture.lower()
-        if _target_gesture == "tpose":
-            return self.isTPose()
-        elif _target_gesture == "fieldgoal":
-            return self.isFieldGoal()
-        elif _target_gesture == "righthandwave":
-            return self.isRightHandRightToLeftWave()
-        elif _target_gesture == "lefthandraise":
-            return self.isRaiseLeftHand()
-        elif _target_gesture == "righthandraise":
-            return self.isRaiseRightHand()
-        else:
-            print("gesture "+_target_gesture+" not recognized.")
-
-    def isRightHandRightToLeftWave(self):
-        x = []
-        y = []
-        if DEBUG_PROCESS_KEYPOINTS:
-            print("Wave - last3frames length: {}".format(len(self.last_3_frames)))
-        
-        # get the right wrist x-y coordinates from the last 3 frames array
-        for i in range(len(self.last_3_frames)):
-            frame = self.last_3_frames[i][1]
-            x.append(frame[0,body25['RWrist'],0])
-            y.append(frame[0,body25['RWrist'],1])
-
-        if DEBUG_PROCESS_KEYPOINTS:
-            print("Wave - processed x: {0}".format(x))
-        
-        npx = np.array([z for z in x if z>0])
-        npy = np.array([z for z in y if z>0])
-
-        # TODO: normalize threshold to skeleton size
-        if len(npy) > 1:
-            # check for minimal y movement
-            if ( ((npy.max() - npy.min())/self.HEIGHT) < 0.3 ):
-                # check for x coordinates are in order which means movement in one directin
-                # check for movement across .4 of the screen width
-                # TODO normalize sizes
-                if ( np.array_equal(np.sort(npx, axis=None)[::-1], npx) ) and ( ((npx.max() - npx.min())/self.WIDTH) > 0.4 ):
-                    return True
-        return False
-
-
-    # checks for arms straight out from sides, using passed in keypoints to class
-    # returns true if in TPose
-    def isTPose(self):
-        #print ("checking for tpose")
-        # 1D array: y of [body25 1 thru 7]
-        parts = [body25[x] for x in ['RWrist','RElbow','RShoulder','Neck','LShoulder','LElbow','LWrist']]
-
-        # get y
-        a = self.keypoints[0, parts, 1].flat
-        TPoseKeypoints_y = a[a > 0]
-        # get x
-        a = self.keypoints[0, parts, 0].flat
-        TPoseKeypoints_x = a[a > 0]
-        
-        if DEBUG_PROCESS_KEYPOINTS:
-            print('tpose - y: {}'.format(self.TPoseKeypoints_y))
-            print('tpose - x: {}'.format(self.TPoseKeypoints_y1))
-
-        # check for at least 6 of the 8 points are detected and their y variation is within threshold
-        threshold = 0.1
-        if (TPoseKeypoints_y.size >= 6) and (abs(((TPoseKeypoints_y.max() - TPoseKeypoints_y.min()) / self.HEIGHT)) < threshold):
-            if DEBUG_PROCESS_KEYPOINTS:
-                print("tpose - testing for x order")
-            if (np.array_equal(np.sort(TPoseKeypoints_x, axis=None), TPoseKeypoints_x)):
-                return True
-        else:
-            return False
-
-    def isFieldGoal(self):
-        # get parts needed for this pose
-        elbow2elbow_indexes = [body25[x] for x in ['Neck','RShoulder','RElbow','LElbow','LShoulder']]
-        a = self.keypoints[0,elbow2elbow_indexes,1].flat
-        self.elbowToElbow_y = a[a>0]
-        
-        # check that elbows and shoulders are horizontal within threshold
-        threshold = 0.2
-        isElbowsCorrect = False
-        if (self.elbowToElbow_y.size >= 4) and (abs(((self.elbowToElbow_y.max() - self.elbowToElbow_y.min()) / self.HEIGHT)) < threshold):
-            isElbowsCorrect = True
-
-        isHandsCorrect = False
-        # check if wrist and elbox x coords are within in threshold
-        if np.count_nonzero(self.keypoints[0,[body25[x] for x in ['RElbow','RWrist','LWrist','LElbow']],0]) == 4:
-            if DEBUG_PROCESS_KEYPOINTS:
-                print('field goal - non zero check')
-            if (abs(self.keypoints[0,body25['RWrist'],0]-self.keypoints[0,body25['RElbow'],0]) / self.WIDTH) < threshold:
-                if DEBUG_PROCESS_KEYPOINTS:
-                    print("field goal - Right pass")
-                if (abs(self.keypoints[0,body25['LWrist'],0]-self.keypoints[0,body25['LElbow'],0]) / self.WIDTH) < threshold:
-                    if DEBUG_PROCESS_KEYPOINTS:
-                        print('field goal - left pass')
-                    # check if wrists are above nose
-                    if (self.keypoints[0,body25['RWrist'],1] < self.keypoints[0,body25['Nose'],1]):
-                        if (self.keypoints[0,body25['LWrist'],1] < self.keypoints[0,body25['Nose'],1]):
-                            isHandsCorrect = True
-
-        return isHandsCorrect and isElbowsCorrect
-
-    #TODO normalize and add checks for other arm is down
-    # left hand raised
-    def isRaiseLeftHand(self):
-        indexes = [body25[x] for x in ['LElbow','LShoulder','LWrist']]
-        a = self.keypoints[0,indexes,1].flat
-        y = a[a>0]
-        a = self.keypoints[0,indexes,0].flat
-        x = a[a>0]
-        
-        #checking for low change in x ==> arm is straight up
-        threshold = 0.2
-        if ( y.size == 3 ) and (abs(((x.max() - x.min()) / self.HEIGHT)) < threshold):
-            # check if wrists are above nose
-            if (self.keypoints[0,body25['LWrist'],1] < self.keypoints[0,body25['Nose'],1]):
-                return True
-
-        return False
-
-    # right hand raised
-    def isRaiseRightHand(self):
-        indexes = [body25[x] for x in ['RElbow','RShoulder','RWrist']]
-        # print(self.keypoints.size)
-        a = self.keypoints[0,indexes,1].flat
-        y = a[a>0]
-        a = self.keypoints[0,indexes,0].flat
-        x = a[a>0]
-        
-        #checking for low change in x ==> arm is straight up
-        threshold = 0.2
-        if ( y.size == 3 ) and (abs(((x.max() - x.min()) / self.HEIGHT)) < threshold):
-            # check if wrists are above nose
-            if (self.keypoints[0,body25['RWrist'],1] < self.keypoints[0,body25['Nose'],1]):
-                return True
-
-        return False
 
 def main():
 
@@ -421,10 +221,10 @@ def main():
             if main_keypoints.size > 1:
                 nose_x = main_keypoints[0][0][0]
                 region = 10 - int(10*nose_x/WIDTH0)
-            # print(region)
             if MQTT_ENABLE:
                 client.publish(localization_topic,  payload= (region), qos=0, retain=False)
-                print (region)
+
+            print(region)
 
         # break loop and exit when ESC key is pressed
         key = cv2.waitKey(20)
